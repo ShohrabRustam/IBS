c3ac519f2df741090d546a072df20344c30d4f4c
Shohrab Rustam <mohdrustam001@gmail.com>
2022-02-23 17:10:13 +0530

    set little bit navbar

---
+++ resources/views/Nav/InsuranceType/bike.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+
+@section('title')
+Insurance
+@endsection
+
+@section('section')
+<h2>Bike Insurance Page</h2>
+
+@endsection
+
---
+++ resources/views/Nav/InsuranceType/car.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+
+@section('title')
+Insurance
+@endsection
+
+@section('section')
+<h2>Car Insurance Page</h2>
+
+@endsection
+
